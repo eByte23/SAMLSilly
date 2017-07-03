b4d88c9306cfa059d43af6f8f6acfc57d4d9b1a6
Elijah Bate <elijah.bate@inlogik.com>
2017-07-03 13:21:27 +1000

    Fix reference id bug and 256 and 512 Signature Support properly

--- src/SAMLSilly.AspNetCoreMiddleware/Core/SAMLConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace SAMLSilly.AspNetCore.Core
-{
-    public static class SAMLConstants
-    {
-        public static string SHA1Dsig => System.Security.Cryptography.Xml.SignedXml.XmlDsigRSASHA1Url;
-
-        public const string SHA256Dsig = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
-    }
-}
